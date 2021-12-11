86ff5e4ca4bc23f9fe9ea41018fe0329f792721a
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2021-12-11 10:59:35 -0500

    Finish assignment

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <!-- <h3 v-if="showGoal">{{ goal }}</h3> -->
+  <h3 v-if="dataGoal.showGoal">{{ dataGoal.goal }}</h3>
+  <h3 v-if="showGoalByOptions">{{ goalByOptions }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="toggleGoal">Toggle Goal</button>
+  <button @click="toggleGoalByOptions">Toggle Goal By Options API</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,8 +16,45 @@
 </template>
 
 <script>
+import { reactive, ref } from 'vue';
+
 export default {
-  
+  setup() {
+    // const goal = ref("My main course goal is to become a full-stack developer (Separate refs)");
+    // const showGoal = ref(false);
+    const dataGoalRef = {
+      goal: ref("My main course goal is to become a full-stack developer (Ref Object)"),
+      showGoal: ref(true),
+    };
+    const dataGoal = reactive({
+      goal: "My main course goal is to become a full-stack developer (Reactive Object)",
+      showGoal: true,
+    });
+
+    const toggleGoal = () => {
+      // showGoal.value = !showGoal.value;
+      dataGoalRef.showGoal.value = !dataGoalRef.showGoal.value;
+      dataGoal.showGoal = !dataGoal.showGoal;
+    };
+
+    return {
+      goal: dataGoalRef.goal,
+      showGoal: dataGoalRef.showGoal,
+      dataGoal,
+      toggleGoal,
+    };
+  },
+  data() {
+    return {
+      goalByOptions: "My main course goal is to become a full-stack developer (Options API)",
+      showGoalByOptions: true,
+    };
+  },
+  methods: {
+    toggleGoalByOptions() {
+      this.showGoalByOptions = !this.showGoalByOptions;
+    },
+  },
 }
 </script>
 
